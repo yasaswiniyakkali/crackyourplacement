26ff3d7e807d3daf47cc025bc17dc82ad586ad29
yasaswini <97458051+yasaswiniyakkali@users.noreply.github.com>
2024-07-17 17:51:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        //code here 
+        vector<long long int> product(n,1);
+        
+        long long int lprod=1;
+        for(int i=0;i<n;i++)
+        {
+            product[i]=lprod;
+            lprod*=nums[i];
+        }
+        long long int rprod=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            product[i]*=rprod;
+            rprod*=nums[i];
+        }
+        return product;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
