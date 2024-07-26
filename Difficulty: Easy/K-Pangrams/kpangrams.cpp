052eadcfb55fdcfe385d32e3e05bdc9c972b04ca
yasaswini <97458051+yasaswiniyakkali@users.noreply.github.com>
2024-07-26 22:46:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-Pangrams/kpangrams.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        // code here
+        int cnt=0;
+        unordered_map<char,int>mp;
+        for(auto&x:str){
+            if(x!=32){
+                cnt++;
+                mp[x]++;}
+            }
+        return cnt>=26 && 26-mp.size()<=k;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        string str;
+        getline(cin, str);
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+        bool a = ob.kPangram(str, k);
+        if (a)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
